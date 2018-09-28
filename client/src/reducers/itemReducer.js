6964b2aa3d8e05a7c65aec7feb2802c38c715f7e
jeff00docusign <jeffrey.tanner@docusign.com>
2018-09-28 12:55:01 -0700

    [Step-8] Cleanup and Scripts

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,36 +1,29 @@
-import uuid from 'uuid';
 
 import {
   GET_ITEMS,
   ADD_ITEM,
   DELETE_ITEM,
   ITEMS_LOADING
-} from '../actions/types';
+} from '../actions/types'
 
 const initialState = {
-  // items: [
-  //     {id: uuid(), name: 'Eggs'},
-  //     {id: uuid(), name: 'Coffee'},
-  //     {id: uuid(), name: 'Water'},
-  //     {id: uuid(), name: 'Candy'}
-  // ]
   items: [],
   loading: false
-};
+}
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-          ...state,
-          items: action.payload,
-          loading: false
-      };
+        ...state,
+        items: action.payload,
+        loading: false
+      }
     case ADD_ITEM:
-          return {
-              ...state,
-              items: [action.payload, ...state.items]
-          }
+      return {
+        ...state,
+        items: [action.payload, ...state.items]
+      }
     case DELETE_ITEM:
       return {
         ...state,
@@ -38,10 +31,10 @@ export default function(state = initialState, action) {
       }
     case ITEMS_LOADING:
       return {
-          ...state,
-          loading: true
-      };
+        ...state,
+        loading: true
+      }
     default:
-      return state;
+      return state
   }
 }
--- server.js
+++ server.js
@@ -32,6 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
